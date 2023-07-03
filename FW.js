7310393056ea99e4feb47dce281e40c701e42ecc
VsangkyeongV <137258368+VsangkyeongV@users.noreply.github.com>
2023-07-03 23:20:34 +0900

    말씀하신 부분 고친 코드입니다
    이상한 브랜치에 올렸었네요 ㅠㅠ

--- FW.js
+++ FW.js
@@ -9,16 +9,22 @@ const buttonAble = () => {
 
   if (id.length > 5 && pw.length > 5) {
     loginButton.disabled = false;
+    loginButton.classList.remove("disabled");
+    loginButton.classList.add("abled");
   } else {
     loginButton.disabled = true;
+    loginButton.classList.remove("abled");
+    loginButton.classList.add("disabled");
   }
 };
 
 const popUp = () => {
   id = idInput.value;
   pw = pwInput.value;
-
   alert("id : " + id + "\npw :" + pw);
+  idInput.value = "";
+  pwInput.value = "";
+  window.location.href = "https://www.google.com";
 };
 
 idInput.addEventListener("input", buttonAble);
